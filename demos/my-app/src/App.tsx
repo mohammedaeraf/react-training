1659c952bbe6bbf3c0ccc0a0e3b4dec4d354ffb9
Mohammed Aeraf <aeraf@outlook.com>
2025-07-27 18:11:37 +0530

    solutions to a01 and a02

--- demos/my-app/src/App.tsx
+++ demos/my-app/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Counter from "./Components/Counter";
+import ProductList from "./Components/ProductList";
 import ToggleMessage from "./Components/ToggleMessage";
 
 function App() {
   return (
     <div id="aeraf-div">
       <h1>My First React Application</h1>
-      <ToggleMessage></ToggleMessage>
+      <ProductList></ProductList>
     </div>
   );
 }
